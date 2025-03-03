27ca7ed877fb1fcbcf01349029a0bd7d7ad6c715
Paolo Magliano <paolo.magliano@outlook.it>
2025-03-03 16:32:45 +0100

    Fix log info

--- experiments/util/log_info.py
+++ experiments/util/log_info.py
@@ -1,5 +1,6 @@
 import wandb
 import os
+import sys
 from math import floor, log10
 
 def wandb_init(cfg_dict):
@@ -8,9 +9,12 @@ def wandb_init(cfg_dict):
     wandb.login(key=key, timeout=10)
     return wandb.init(project=cfg_dict['wandb']['project'], dir=cfg_dict['results_dir'], config=cfg_dict, group=cfg_dict['wandb']['group'], mode=mode, entity=cfg_dict['wandb']['entity'])
 
-def log_info(logger, rl_agent, epoch, J, R, E, V, task_info, precision=5, log_agent=False):
+def log_info(logger, rl_agent, J, R, E, V, task_info, epoch=None, precision=5, log_agent=False):
     log_dict = {"Reward/J": sig_figs(J, precision), "Reward/R": sig_figs(R, precision), "Training/E": sig_figs(E, precision), "Training/V": sig_figs(V, precision), "Episode_lenght": sig_figs(task_info['episode_length'], precision)}
-    logger.epoch_info(epoch + 1, **{'J': sig_figs(J, precision), 'R': sig_figs(R, precision), 'E': sig_figs(E, precision), 'V': sig_figs(V, precision), 'Episode_lenght': sig_figs(task_info['episode_length'], precision)})
+    if epoch is not None:
+        logger.epoch_info(epoch + 1, **{'J': sig_figs(J, precision), 'R': sig_figs(R, precision), 'E': sig_figs(E, precision), 'V': sig_figs(V, precision), 'Episode_lenght': sig_figs(task_info['episode_length'], precision)})
+    else:
+        logger.info(f'J: {sig_figs(J, precision)} R: {sig_figs(R, precision)} E: {sig_figs(E, precision)} V: {sig_figs(V, precision)} Episode_lenght: {sig_figs(task_info["episode_length"], precision)}')
     for name, value in task_info['constraints'].items():
         costr_dict_wb = {}
         costr_dict = {}
@@ -19,7 +23,10 @@ def log_info(logger, rl_agent, epoch, J, R, E, V, task_info, precision=5, log_ag
             costr_dict[f"{name}/{k}"] = sig_figs(v, precision)
         if log_agent and value['num_violation'] > 0 and epoch > 0:
             logger.log_agent(rl_agent, str(epoch + 1) + '_' + name)
-        logger.epoch_info(epoch + 1, **costr_dict)
+        if epoch is not None:
+            logger.epoch_info(epoch + 1, **costr_dict)
+        else:
+            logger.info(f'{name}: {costr_dict}')
         log_dict.update(costr_dict_wb)
     if len(task_info) > 1:
         logger.info('#' * 100)
@@ -31,3 +38,12 @@ def sig_figs(x: float, precision: int):
     precision = int(precision)
 
     return round(x, -int(floor(log10(abs(x)))) + (precision - 1)) if x != 0 else 0
+
+def clean_dir(dir_path):
+    print(f'Cleaning directory {dir_path}: {os.path.exists(dir_path)} {os.path.isdir(dir_path)} {os.listdir(dir_path)}')
+    if os.path.exists(dir_path) and os.path.isdir(dir_path) and not os.listdir(dir_path):
+        os.rmdir(dir_path)
+
+def clean_exit(sig, frame, dir_path):
+    clean_dir(dir_path)
+    sys.exit(0)
