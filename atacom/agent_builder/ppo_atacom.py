55a25bf0ab3e69a713387f94b3259470c15b3d65
Paolo Magliano <paolo.magliano@outlook.it>
2025-03-12 14:51:29 +0100

    Implement loss plot

--- atacom/agent_builder/ppo_atacom.py
+++ atacom/agent_builder/ppo_atacom.py
@@ -1,5 +1,5 @@
 import torch
-
+import matplotlib.pyplot as plt 
 
 from mushroom_rl.algorithms.actor_critic.deep_actor_critic.ppo_nikita import NikitaPPO
 from mushroom_rl.rl_utils.value_functions import compute_gae
@@ -13,6 +13,9 @@ class AtacomPPO(NikitaPPO):
             n_epochs_policy, batch_size, eps_ppo, lam, ent_coeff,
             critic_fit_params, clip_grad_norm, schedule, desired_kl)
         self._atacom_enable = atacom_enable
+
+        self._loss_clip = []
+        self._loss_entropy = []
 
     def fit(self, dataset):
         state, action, reward, next_state, absorbing, last = dataset.parse(to='torch')
@@ -35,7 +38,41 @@ class AtacomPPO(NikitaPPO):
         self._V.fit(state, v_target, **self._critic_fit_params)
 
         self._update_policy(state, action, adv, old_log_p, state, old_pol_dist)
+        self._plot_loss()
 
         # Print fit information
         self._log_info(dataset, state, v_target, old_pol_dist)
-        self._iter += 1
+        self._iter += 1        
+
+    def _update_policy(self, obs, act, adv, old_log_p, state, old_pol_dist):
+        for epoch in range(self._n_epochs_policy()):
+            for obs_i, act_i, adv_i, old_log_p_i in minibatch_generator(
+                    self._batch_size(), obs, act, adv, old_log_p):
+                with torch.inference_mode():
+                    new_pol_dist = self.policy.distribution_t(state)
+                    kl = torch.mean(torch.distributions.kl.kl_divergence(old_pol_dist, new_pol_dist))
+                    self._adapt_learning_rate(kl)
+
+                self._optimizer.zero_grad()
+                prob_ratio = torch.exp(self.policy.log_prob_t(obs_i, act_i) - old_log_p_i)
+                clipped_ratio = torch.clamp(prob_ratio, 1 - self._eps_ppo(), 1 + self._eps_ppo.get_value())
+                loss_clip = -torch.mean(torch.min(prob_ratio * adv_i, clipped_ratio * adv_i))
+                loss_entropy -= self._ent_coeff() * self.policy.entropy_t(obs_i)
+                self._loss_clip.append(loss_clip.item())
+                self._loss_entropy.append(loss_entropy.item())
+                loss = loss_clip + loss_entropy
+
+                loss.backward()
+                self._clip_gradient()
+                self._optimizer.step()
+
+    def _plot_loss(self):
+        if self._iter % 10 != 0:
+            return 
+        plt.plot(self._loss_clip)
+        plt.plot(self._loss_entropy)
+        plt.savefig(f'plot/loss/loss_{self._iter}.png')
+        plt.close()
+
+        self._loss_clip = []
+        self._loss_entropy = []
