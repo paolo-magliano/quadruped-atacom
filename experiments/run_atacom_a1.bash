a42eb57b126c3ea013bff4af7fa0aed27327475a
Paolo Magliano <paolo.magliano@outlook.it>
2025-03-12 13:39:15 +0100

    Add RL Agent 1.5

--- experiments/run_atacom_a1.bash
+++ experiments/run_atacom_a1.bash
@@ -10,5 +10,6 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate env_isaacsim
-python experiments/run_exp_a1.py env_type=PD constraints.joint_limit=[1.]
 python experiments/run_exp_a1.py env_type=PD constraints.joint_limit=[2.]
+python experiments/run_exp_a1.py env_type=PD constraints.joint_limit=[1.]
+python experiments/run_exp_a1.py env_type=PD constraints.joint_limit=[0.5]
